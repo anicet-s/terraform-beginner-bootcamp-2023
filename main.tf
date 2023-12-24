e6be860b5c99f711bef4e8c17cf0914a2bcc37c5
tghil7 <tghil7@aol.com>
2023-12-24 22:40:49 +0000

    Terraform Random Bucket Name #10

--- main.tf
+++ main.tf
@@ -1,1 +1,36 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.31.0"
+    }
+
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length           = 16
+  special          = false
+}
+
+output "bucket_name_random"{
+    value =random_string.bucket_name.id
+}
+
+
+
+
+
