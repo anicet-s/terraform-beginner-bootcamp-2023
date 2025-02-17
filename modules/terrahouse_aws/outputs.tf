cb3682c01b24aaeb05210542b81e4baf3fddf16e
Anicet <anicet@bdesmarest.onmicrosoft.com>
2025-02-16 21:57:20 -0600

    Update to cidr_ipv4

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -1,12 +1,13 @@
-output "bucket_name_random"{
-    value =random_string.bucket_name.id
-}
-
 output "bucket_name" {
     value = aws_s3_bucket.my_bucket.bucket
 } 
 
 output "user_uuid" {
     value = var.user_uuid
+}
+
+output "s3_website_endpoint" {
+    description = "S3 static website endpoint"
+    value = aws_s3_bucket.my_bucket.website_endpoint
 }
 
